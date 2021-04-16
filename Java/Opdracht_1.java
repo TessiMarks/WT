09417b05c363780d8160eb6d326b7c762113c6b9
TessiMarks <tessimariamarks@gmail.com>
2021-04-16 14:35:55 +0200

    opdrachten Java gewerkt

---
+++ Java/Opdracht_1.java
@@ -0,0 +1,11 @@
+class Opdracht1 {
+    public static void main(String[] args) {
+        System.out.println("OPDRACHT 1");
+        String str1 = "wor";
+        String str2 = "tel";
+        String str3 = "tjes";
+        String str4 = "taart";
+        String result = str1 + str2 + str3 +str4;
+        System.out.println(result);
+    }   
+}
